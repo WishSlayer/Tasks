0f1655f27ff16df964d1f61fccfd611cd2dbcbf3
WishSlayer <wishslayer@icloud.com>
2024-04-11 18:42:05 +0300

    v1

---
+++ task14.js
@@ -0,0 +1,4 @@
+let root = document.querySelector('div[name="area"]')
+let bg = document.createElement('div')
+bg.classList.add('fixed', 'w-full', 'h-full', 'flex', 'bg-black', 'z-100', 'opacity-80')
+root.appendChild(bg)
